c3b442d4db78e0a6fc8c72c42f087249e837bb48
Ben Hessel <hesselfitness@gmail.com>
2022-07-05 10:08:03 -0400

    themecontext test

---
+++ src/ContextComponent.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useTheme, useThemeUpdate } from './ThemeContext';
+
+const Contextcomponent = () => {
+
+    const darkTheme = useTheme()
+    const toggleTheme = useThemeUpdate()
+    const themeStyles = {
+        backgroundColor: darkTheme ? '#333' : '#CCC',
+        color: darkTheme ? '#CCC' : '#333',
+        padding: '2rem',
+        margin: '2rem'
+    }
+
+    return (
+        <>
+            <button onClick={toggleTheme}>Toggle Btn</button>
+            <div style={themeStyles}>
+                Theme Color
+            </div>
+
+        </>
+    );
+}
+
+export default Contextcomponent;
